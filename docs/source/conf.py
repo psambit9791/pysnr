61d67ba49d79ce6bf5bef60fe94bff3332963816
psambit9791 <sambitpaul1992@gmail.com>
2022-06-17 14:48:30 +0100

    Docs deployment Bug fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,21 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.napoleon', 'sphinx.ext.imgmath']
+extensions = [
+    'sphinx.ext.napoleon',
+    'sphinx.ext.imgmath',
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
